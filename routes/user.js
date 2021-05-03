bc775082d515da11ba269e90a4aae6e40f09c95c
Guilherme Cassiano <guilherme.cassiano.developer@gmail.com>
2021-05-03 07:17:21 -0300

    API pronta pra um comeco

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 //INSERE UM USUARIO
 router.post('/cadastro', (req, res, next) =>{
     mysql.getConnection((error, conn) =>{
-        conn.query(
-            'INSERT INTO users (email, senha) VALUES (?,?)',
-            [req.body.email ,req.body.senha],
-            (error, results) =>{
-                conn.release();
-        
-                if(error){return res.status(500).send({error: error})}
-
-                response = {
-                    mensagem: 'Usuario criado com sucesso',
-                    usuarioCriado: {
-                        userId: results.insertId,
-                        email: req.body.email
-                    }
-                }
-                        res.status(201).send(response);
-            }
-        )    
-        
-        })    
-})
+        if(error){return res.status(500).send({message: error})}
+        conn.query('SELECT * FROM users WHERE email = ?',[req.body.email], (error,results)=>{
+            if(error){return res.status(500).send({message: error})}
+            if(results.length > 0){
+                res.status(409).send({message: 'Usuário já cadastrado'})
+            }else{
+                bcrypt.hash(req.body.senha, 10,(errBcrypt, hash)=>{
+                    if(errBcrypt){return res.status(500).send({ error : errBcrypt})}
+                    conn.query(
+                        'INSERT INTO users (email, senha) VALUES (?,?)',
+                        [req.body.email ,hash],
+                        (error, results) =>{
+                            conn.release();
+                    
+                            if(error){return res.status(500).send({error: error})}
+            
+                            const token = jwt.sign({
+                                userId: results.insertId,
+                                email: req.body.email
+                            },process.env.JWT_KEY,
+                            {
+                                expiresIn: '1h'
+                            })
+    
+                            res.status(201).send({token: token});
+                        }
+                    );    
+                });  
+            };      
+        });                  
+    });
+});
 router.post('/login',(req,res,next)=>{
     mysql.getConnection((error,conn)=>{
         if(error){return res.status(500).send({error: error})}
@@ -34,17 +47,30 @@ router.post('/login',(req,res,next)=>{
             conn.release();
             if(error){ return  res.status(500).send({error: error})};
             if(results.length === 0){
-                return res.status(404).send ({message: 'E-mail  não encontrado' })
+                return res.status(401).send ({message: 'E-mail  não encontrado' })
             };
-            if(req.body.senha === results[0].senha){
-                return res.status(200).send({message: 'Autenticado com sucesso'})
-            }else{
-                return res.status(401).send({message: 'Falha na Autenticação'})
-            }
+            bcrypt.compare(req.body.senha, results[0].senha,(err,result)=>{
+                if(err){
+                    return res.status(401).send({message: 'Falha na auteticação'})
+                }
+                if(result){
+                    const token = jwt.sign({
+                        userId: results[0].userId,
+                        email: results[0].email
+                    },process.env.JWT_KEY,
+                    {
+                        expiresIn: '1h'
+                    })
+                    return res.status(200).send({
+                        //message:'Autenticado com sucesso',
+                        token  :token                            
+                    })
+                }
+                return res.status(401).send({message:'Falha na autenticação'})
+            })
         
-        })
-    })
-})
-
+        });
+    });
+});
 
 module.exports = router;
